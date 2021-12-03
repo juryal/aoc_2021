e4da50798f59cec759f910c25b3190af55813c82
Justin Alexander <justin@justinalexander.us>
2021-12-02 20:17:45 -0500

    day 2 part 2

--- 02/aoc02.py
+++ 02/aoc02.py
@@ -11,9 +11,26 @@ def part_one(file):
         elif instruction[0] == "up":
             depth -=int(instruction[1])
     return horizontalposition * depth
+
+def part_two(file):
+    horizontalposition = 0
+    depth = 0
+    aim = 0
+    for line in file:
+        instruction = tuple(line.strip("\r\n").split())
+        if instruction[0] == "forward":
+            speed = int(instruction[1])
+            horizontalposition += speed
+            depth = depth + (aim * speed)
+        elif instruction[0] == "down":
+            aim +=int(instruction[1])
+        elif instruction[0] == "up":
+            aim -=int(instruction[1])
+    return horizontalposition * depth
+
 file = open("data.txt")
-mode = 1
+mode = 2
 if mode == 1:
     print(part_one(file))
-#if mode == 2:
-#    print(part_two(file))
+if mode == 2:
+    print(part_two(file))
